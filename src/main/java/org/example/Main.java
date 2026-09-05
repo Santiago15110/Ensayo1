5e97f31d84760ca3dce93a9f9d73ccbf101f5444
sxnti328 <santiago.huertasb@uqvirtual.edu.co>
2026-09-04 22:15:47 -0500

    agregue nuevos cambios

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.print("David Santiago Mamian");
+        System.out.print("Santiago Huertas Bermudez");
+
     }
 }
